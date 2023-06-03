dee9f5913b72b7ef2e902250305dd5a6625c18a3
buiphuckien <buiphuckien@gmail.com>
2023-06-04 06:52:52 +0700

    Add form to upload file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
 # import pandas as pd
 import numpy as np
 # import sklearn
 import joblib
-from flask import Flask, render_template, request
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/') # to homepage
 def home():
@@ -25,4 +28,41 @@ def predict():
         return render_template('predict.html', pred='None')
 
 
-        
+
+
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
