b41658fc185d53eed227b7342105e659c986fb4d
buiphuckien <buiphuckien@gmail.com>
2024-02-19 17:25:56 +0700

    Add dashboard and form.

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+from wtforms import FloatField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+
+
 
 import pandas as pd
 import numpy as np
@@ -8,10 +14,21 @@ import sklearn
 import joblib
 from PIL import Image
 from dnn_app_utils_v3 import predict, L_model_forward
+import json
+import plotly
+import plotly.express as px
+
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Hard to guess string'
+
+bootstrap = Bootstrap(app)
+
+
 
 @app.route('/') # to index page
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/about') # to about
@@ -29,6 +46,38 @@ def post():
 @app.route('/math') # to math
 def math():
     return render_template('math.html')
+
+
+
+x = np.arange(10)
+y = 2*x
+data = {'x':x, 'y':y}
+iris = pd.read_csv("data/iris.csv")
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    df = px.data.medals_wide() # this data comes from plotly library
+
+    # Plot using plotly package
+    fig1 = px.bar(df, x = "nation", y = ['gold', 'silver', 'bronze'], title='Wide-Form Input')
+
+    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Plot figure 2
+    fig2 = px.line(data_frame=data, x="x", y="y")
+    graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Plot figure 3 - iris
+    fig3 = px.scatter_3d(iris, x='sepal_length', y='sepal_width', z='petal_width',
+              color='species')
+    graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("dashboard.html", title="Index"\
+                            , graph1JSON=graph1JSON\
+                            , graph2JSON=graph2JSON\
+                            , graph3JSON=graph3JSON)
 
 @app.route('/predict_lr', methods=['GET', 'POST'])
 def predict_lr():
@@ -56,6 +105,45 @@ def predict_lr():
     
     else:
         return render_template('predict_lr.html')
+
+
+
+
+class LinearRegressionForm(FlaskForm):
+    a = FloatField('a = ?')
+    b = FloatField('b = ?')
+    submit = SubmitField('Submit')
+
+
+@app.route('/predict_lr1', methods=['GET', 'POST'])
+def predict_lr1():
+
+    # Create form instance
+    form = LinearRegressionForm()
+
+    if form.validate_on_submit(): # If there is a submit
+
+        # Get values from form
+        a = form.a.data # the value of a
+        b = form.b.data # the value of b
+
+        # Putting a, b into an array
+        test = np.array([a, b]) # shape = (2,)
+        test = test.reshape(1, -1) # shape = (1, 2)
+
+        # Loading the model
+        model = open('lr_model.pkl', 'rb') # open the model
+        lr_model = joblib.load(model) # load model
+
+        # Make a prediction
+        pred  = lr_model.predict(test)
+        pred = pred.item() # Just
+
+        # Give answer to a prediction
+        return render_template('predict_lr1.html', form=form, pred=pred)
+    
+    else:
+        return render_template('predict_lr1.html', form=form)
 
 
 
